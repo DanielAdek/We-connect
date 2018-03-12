97c71be3587386a87c0d9a2b66c13e8bc5cd027e
Daniel Adek <maildaniel.me1@gmail.com>
2018-03-12 21:06:57 +0100

    chore: set server app

---
+++ server/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import routes from './routers';
+
+// create an instance of express
+const app = express();
+
+// log request to console
+app.use(logger('dev'));
+
+//use body parser to parse json format
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/', routes);
+
+export default app;
