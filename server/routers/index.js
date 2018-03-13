23cc962cb8994d6e8523fffd7b492e770a314fee
Daniel Adek <maildaniel.me1@gmail.com>
2018-03-13 12:03:27 +0100

    feature: add update business route

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import users from './../controllers/usersController';
+import business from './../controllers/businessController';
 
 const router = express.Router();
 
 router.post('/api/v1/auth/signup', users.createUser);
 router.post('/api/v1/auth/login', users.loginUser);
 router.post('/api/v1/business', users.createBusiness);
+router.put('/api/v1/business/:businessid', business.updateBusiness);
 
 export default router;
