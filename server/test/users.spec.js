865f06bd13f527ea53822e02cda02b8906c372d3
Daniel Adek <maildaniel.me1@gmail.com>
2018-03-13 22:57:20 +0100

    chore: test sign up API

---
+++ server/test/users.spec.js
@@ -0,0 +1,48 @@
+import app from '../app';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import expect from 'chai';
+import assert from 'chai';
+
+chai.use(chaiHttp);
+const request = chai.request(app);
+const should = chai.should();
+
+describe('Test all users APIs', ()=> {
+    describe('/POST route', ()=> {
+        const new_user = {
+            username:'john',
+            email:'johndoe@example.com',
+            password: 'chairate'
+        };
+        it('should return successfull with status 200', (done)=> {
+            const username = 'john';
+            request
+                .post('/api/v1/auth/signup')
+                .send(new_user)
+                .end((err, res)=>{
+                    // expect(res.status()).to.be.equal(200);
+                    res.should.have.status(201);
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('user');
+                    res.body.user.should.be.equal(`${username} is successfully created as a new user`);
+                    done();
+                });
+        });
+
+        it('should return bad request with 400 status code', (done)=>{
+            request
+                .post('/api/v1/auth/signup')
+                .send()
+                .end((err, res)=> {
+                    res.should.have.status(400);
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('message');
+                    res.body.message.should.be.equal('Input field cannot be empty');
+                    res.body.should.have.property('err')
+                    res.body.err.should.be.eql(true);
+                    done()
+                })
+        })
+    });
+});
