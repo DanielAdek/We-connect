2108f3e93685ac0d6ec3ed80e570e51c9d5e82fc
Daniel Adek <maildaniel.me1@gmail.com>
2018-03-13 10:50:44 +0100

    feature: create login route

--- server/routers/index.js
+++ server/routers/index.js
@@ -4,5 +4,6 @@ import users from './../controllers/usersController';
 const router = express.Router();
 
 router.post('/api/v1/auth/signup', users.createUser);
+router.post('/api/v1/auth/login', users.loginUser);
 
 export default router;
