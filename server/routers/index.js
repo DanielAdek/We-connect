145394e24c595ae1562535caee32143b2aad8734
Daniel Adek <maildaniel.me1@gmail.com>
2018-03-13 12:30:47 +0100

    feature: added delete route

--- server/routers/index.js
+++ server/routers/index.js
@@ -8,5 +8,6 @@ router.post('/api/v1/auth/signup', users.createUser);
 router.post('/api/v1/auth/login', users.loginUser);
 router.post('/api/v1/business', users.createBusiness);
 router.put('/api/v1/business/:businessid', business.updateBusiness);
+router.delete('/api/v1/business/:businessid', users.destroyBusiness);
 
 export default router;
