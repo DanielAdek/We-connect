5fcdf007fffb791685ee32eaed31442f803bca07
Daniel Adek <maildaniel.me1@gmail.com>
2018-03-14 14:26:19 +0100

    chore: test api delete route

--- server/test/business.spec.js
+++ server/test/business.spec.js
@@ -30,4 +30,25 @@ describe('/Test all routes on business', () => {
                 });
         })
     })
+
+    describe.only('/DELETE route destroy business', () => {
+        it('should delete a business and return 200 status code', (done) => {
+            request
+                .delete('/api/v1/business/1')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.eql('successfully deleted')
+                    done();
+                });
+        });
+
+        it('should return 404 page not found and 404 status code', (done) => {
+            request
+                .delete('/api/v1/business/90')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        })
+    })
 })
