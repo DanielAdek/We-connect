fa23ef5340f56422ff09833e863b11d7997686ab
Daniel Adek <maildaniel.me1@gmail.com>
2018-03-19 13:05:46 +0100

    installed swagger package

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import routes from './routers';
-
+ 
 // create an instance of express
 const app = express();
 
