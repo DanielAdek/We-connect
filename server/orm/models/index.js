723b36e1792d6ac2c944a0b46ebab50924d62fd8
Daniel Adek <maildaniel.me1@gmail.com>
2018-03-21 10:58:46 +0100

    modify model/index.js file to es6

--- server/orm/models/index.js
+++ server/orm/models/index.js
@@ -1,26 +1,30 @@
-'use strict';
+import fs        from 'fs';
+import path      from 'path';
+import Sequelize from 'sequelize';
+import configPath from './../config/config.json';
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/..\config\config.json')[env];
-var db        = {};
+const env       = process.env.NODE_ENV || 'development';
+const config = configPath[env];
+const basename  = path.basename(module.filename);
+const db        = {};
 
+let sequelize;
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+  );
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
+  .filter(file =>
+    (file.indexOf('.') !== 0) &&
+    (file !== basename) &&
+    (file.slice(-3) === '.js'))
   .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
@@ -30,7 +34,16 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.log(`Unable to connect to the database:, ${err.message}`);
+  });
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
