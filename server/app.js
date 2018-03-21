b55895073dfa51d4b84deef6da23e6218abb781a
Daniel Adek <maildaniel.me1@gmail.com>
2018-03-21 13:40:19 +0100

    fix: wrong naming of files

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import routes from './routers';
+import routes from './routes';
  
 // create an instance of express
 const app = express();
